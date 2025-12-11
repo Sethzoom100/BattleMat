4f1c83bf813acc1a71024941bf43815098c61ae8
Sethzoom100 <76795116+Sethzoom100@users.noreply.github.com>
2025-12-11 15:13:43 -0600

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -9,6 +9,14 @@ const UserSchema = new mongoose.Schema({
     gamesPlayed: { type: Number, default: 0 },
     commanderDamageDealt: { type: Number, default: 0 }
   },
+  decks: [{
+    name: { type: String, required: true },
+    commander: { type: String, required: true },
+    image: { type: String }, // URL to commander art
+    wins: { type: Number, default: 0 },
+    losses: { type: Number, default: 0 },
+    createdAt: { type: Date, default: Date.now }
+  }],
   createdAt: { type: Date, default: Date.now }
 });
 
