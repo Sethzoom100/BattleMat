f2f49379cb8b71a0f959540f03b17e93a9858ec4
Sethzoom100 <76795116+Sethzoom100@users.noreply.github.com>
2025-12-13 09:10:22 -0600

    Update Group.js

--- server/models/Group.js
+++ server/models/Group.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 const GroupSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  code: { type: String, required: true, unique: true }, // Unique 6-digit code
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // List of users
+  code: { type: String, required: true, unique: true }, 
+  members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  admins: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // --- NEW: Admin List
   createdAt: { type: Date, default: Date.now }
 });
 
